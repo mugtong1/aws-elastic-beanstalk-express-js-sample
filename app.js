03c7df637cc9fd86402bb90b1e96d936fe37135a
mugtong1 <mugtong1@naver.com>
2022-09-30 07:30:18 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Hongsun'));
+app.get('/', (req, res) => res.send('Hello Hongsun Kim'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
